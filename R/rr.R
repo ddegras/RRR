c07706ea4813aa24ce1d5b103c2cd928a8872151
David Degras <david.degras@umb.edu>
2025-08-26 12:10:01 -0400

    Update rr.R

--- R/rr.R
+++ R/rr.R
@@ -18,32 +18,17 @@ if (is.null(r)) {
 nr <- length(r)
 rmax <- max(r)
 
-## Trivial case
-if (all(x == 0)) {
-	return(list(coeffs = matrix(0, p, q), fitted = matrix(0, n, q), r = 1))
-}
-
 ## OLS fit
 olsfit <- suppressWarnings(lsfit(x, y, intercept = FALSE))
 bols <- olsfit$coef
 yols <- y - olsfit$residuals
-
-# svdx <- if (maxsvd(x)
-# keep <- (svdx$d >= max(svdx$d[1]*1e-8, 1e-14))
-# if (!all(keep)) {
-	# svdx$u <- svdx$u[,keep]
-	# svdx$d <- svdx$d[keep]
-	# svdx$v <- svdx$v[,keep]
-# }
-# uty <- crossprod(svdx$u,y)
-# bols <- svdx$v %*% (uty / svdx$d) # regression coeffs
-# yols <- svdx$u %*% uty # fitted values
 if (all(r == rankmax)) 
-	return(list(coeffs = bols, fitted = yols, r = r))
+	return(list(coef = bols, fitted = yols, r = r))
 
 ## Projections on lower-rank spaces
-coeffs <- array(dim = c(p, q, nr))
-fitted <- array(dim = c(n, q, nr))
+coeffs <- array(, c(p, q, nr), list(pred = NULL, resp = NULL, r = r))
+fitted <- array(, c(n, q, nr), list(case = NULL, resp = NULL, r = r))
+	
 svdyols <- tryCatch(supressWarnings(svds(yols, k = rmax, nu = 0)), 
 	error = function(e) NULL)
 if (is.null(svdyols)) svdyols <- svd(yols, nu = 0, nv = rmax)
@@ -59,13 +44,13 @@ for (i in 1:nr) {
 }
 
 ## Reduce output dimension if single r value
-if (nr == 1) {
-	dim(coeffs) <- c(p, q)
-	dim(fitted) <- c(n, q)
-} else {
-	dimnames(coeffs) <- list(pred = NULL, resp = NULL, r = r)
-	dimnames(fitted) <- list(case = NULL, resp = NULL, r = r)	
-}
+# if (nr == 1) {
+	# dim(coeffs) <- c(p, q)
+	# dim(fitted) <- c(n, q)
+# } else {
+	# dimnames(coeffs) <- list(pred = NULL, resp = NULL, r = r)
+	# dimnames(fitted) <- list(case = NULL, resp = NULL, r = r)	
+# }
 
 list(coef = coeffs, fitted = fitted, r = r)
 }
