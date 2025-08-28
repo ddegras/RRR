ef15c9fa6b00e65cd656952f72e46f7526f4ccf3
David Degras <david.degras@umb.edu>
2025-08-27 22:17:55 -0400

    Update rr.R

--- R/rr.R
+++ R/rr.R
@@ -18,18 +18,39 @@ if (is.null(r)) {
 nr <- length(r)
 rmax <- max(r)
 
-## OLS fit
-olsfit <- suppressWarnings(lsfit(x, y, intercept = FALSE))
-bols <- olsfit$coef
-yols <- y - olsfit$residuals
-if (all(r == rankmax)) 
+## OLS fit: minimum-norm solution
+olsfit <- NULL
+if (n >= p) {
+	olsfit <- suppressWarnings(lsfit(x, y, intercept = FALSE))
+	if (olsfit$qr$rank == p) {
+		bols <- olsfit$coef
+		pivot <- olsfit$qr$pivot
+		if (!identical(pivot, 1:p)) bols[pivot,] <- bols
+		yols <- y - olsfit$residuals
+	} else {
+		olsfit <- NULL
+	}
+}
+if (is.null(olsfit)) {
+	svdx <- svd(x)
+	pos <- (svdx$d > max(svdx$d[1] * 1e-8, 1e-14))
+	if (any(pos)) {
+		uty <- crossprod(svdx$u[,pos], y)
+		bols <- svdx$v[,pos] %*% (uty / svdx$d[pos])
+		yols <- svdx$u[,pos] %*% uty
+	} else {
+		bols <- rep(0, p)
+		yols <- matrix(0, n, q)
+	}
+}
+if ((nr == 1) && (r == rankmax)) 
 	return(list(coef = bols, fitted = yols, r = r))
 
 ## Projections on lower-rank spaces
 coeffs <- array(, c(p, q, nr), list(pred = NULL, resp = NULL, r = r))
 fitted <- array(, c(n, q, nr), list(case = NULL, resp = NULL, r = r))
 	
-svdyols <- tryCatch(supressWarnings(svds(yols, k = rmax, nu = 0)), 
+svdyols <- tryCatch(suppressWarnings(svds(yols, k = rmax, nu = 0)), 
 	error = function(e) NULL)
 if (is.null(svdyols)) svdyols <- svd(yols, nu = 0, nv = rmax)
 for (i in 1:nr) {
